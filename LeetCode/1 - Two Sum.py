09c7be4f0e8af0a226760e977cc58cf82f96be4c
Priyanshu Saxena <53018678+PriyanshuSaxena2612@users.noreply.github.com>
2026-05-14 11:47:30 +0530

    Create 1 - Two Sum.py

---
+++ LeetCode/1 - Two Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      # first method
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+      # second method 
+      hash_map = {}
+        n = len(nums)
+        for i in range(n):
+            remaining = target - nums[i]
+            if remaining in hash_map:
+                return [hash_map[remaining],i]
+            hash_map[nums[i]] = i
